8968256391a19486edce3b7b39d7b8fb43408f5c
breno nogueira <breno.nog.araujo@hotmail.com>
2022-05-10 22:36:48 -0400

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+app.use(express.json())
+app.use(categoriesRoutes)
+
+app.listen(3333, () => console.log("server is running"));
+
